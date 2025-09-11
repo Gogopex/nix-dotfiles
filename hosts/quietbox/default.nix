8117acb6a1b0d9336bfe4759b9aded34b4e82bda
ludwig <gogopex@gmail.com>
2025-09-11 14:19:42 +0000

    Simplify quietbox config for standalone home-manager

--- hosts/quietbox/default.nix
+++ hosts/quietbox/default.nix
@@ -1,46 +1,38 @@
 lib:
-lib.nixosSystem' (
-  { lib, pkgs, config, inputs, ... }:
-  let
-    inherit (lib) collectNix remove;
-  in
-  {
-    imports = collectNix ./. |> remove ./default.nix;
-
-    class = "nixos";
-    type = "server";  # Server type for headless system
-
-    networking.hostName = "quietbox";
-    system.primaryUser = "ludwig";
-
-    # Shell configuration
-    userShell = "fish";
+let
+  inherit (lib)
+    inputs
+    collectNix
+    remove
+    ;
+  
+  system = "x86_64-linux";
+  
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+  
+  # Collect common modules (but filter out Darwin-specific ones)
+  commonModules = collectNix ../../modules/common 
+    |> remove ../../modules/common/home-manager.nix;  # This is for Darwin/NixOS systems
+  
+  # Collect local modules for this host
+  localModules = collectNix ./. |> remove ./default.nix;
+  
+  homeConfig = inputs.home-manager.lib.homeManagerConfiguration {
+    inherit pkgs;
     
-    # NixOS system configuration (minimal for home-manager only)
-    system.stateVersion = "24.11";
-    nixpkgs.hostPlatform = "x86_64-linux";
+    extraSpecialArgs = {
+      inherit inputs lib;
+      zjstatus = inputs.zjstatus;
+    };
     
-    # Minimal boot configuration
-    boot.loader.grub.enable = false;
-    fileSystems."/" = { device = "/dev/null"; };
-
-    # User configuration
-    users.users.ludwig = {
-      isNormalUser = true;
-      home = "/home/ludwig";
-      shell = if config.userShell == "nushell" then pkgs.nushell else pkgs.fish;
-    };
-
-    # Home-manager configuration
-    home-manager.users.ludwig = {
-      home = {
-        stateVersion = "24.11";
-        homeDirectory = "/home/ludwig";
-        username = "ludwig";
-      };
-      
-      # Set helix as default editor for headless server
-      programs.helix.defaultEditor = true;
-    };
-  }
-)
+    modules = commonModules ++ localModules;
+  };
+in
+{
+  # This marks it as a Linux system for the flake logic
+  class = "nixos";
+  config = homeConfig;
+}
