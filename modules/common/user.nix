03157712878f2c5915de1983e4ee0c482008a042
ludwig <gogopex@gmail.com>
2025-09-17 17:34:56 +0000

    fix

--- modules/common/user.nix
+++ modules/common/user.nix
@@ -1,31 +1,22 @@
 { lib, ... }:
 let
-  inherit (lib) mkOption types mkValue;
+  inherit (lib) mkOption types;
 in
 {
   options.user = {
     name = mkOption {
       type = types.str;
       default = "ludwig";
-      description = "Username for git, jujutsu, and other tools";
     };
 
     email = mkOption {
       type = types.str;
       default = "gogopex@gmail.com";
-      description = "Email address for git, jujutsu, and other tools";
     };
 
     editor = mkOption {
       type = types.str;
       default = "hx";
-      description = "Default editor";
     };
-  };
-
-  config.user = mkValue {
-    name = "ludwig";
-    email = "gogopex@gmail.com";
-    editor = "hx";
   };
 }
